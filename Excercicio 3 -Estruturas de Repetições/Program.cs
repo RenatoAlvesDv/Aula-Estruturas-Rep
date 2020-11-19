c3d4a72b2141c742214446573f49c8824c8ffa20
Your Name <renato_inovacao@hotmail.com>
2020-11-19 02:39:11 -0300

    Exercícico 3 falta concluir

---
+++ Excercicio 3 -Estruturas de Repetições/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Excercicio_3__Estruturas_de_Repetições
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                string nome;
+                int idade; 
+
+                Console.WriteLine("Digite seu Nome:");
+                nome = Console.ReadLine();
+
+                Console.WriteLine("Digite sua Idade:");
+                idade = int.Parse(Console.ReadLine());
+                
+
+        }
+    }
+}
