bb115dbeaa2549226e3afa068734c4978f1a2a17
Your Name <renato_inovacao@hotmail.com>
2020-11-19 02:35:39 -0300

    Exercício 2

---
+++ Exercicio 2 -Estruturas de Repetições/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exercicio_2_dia_18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Faça um programa que leia um nome de usuário e a sua senha e não aceite a senha igual ao nome do usuário, mostrando uma mensagem de erro e voltando a pedir as informações.
+             Console.WriteLine("Cadastre-se");
+             Console.WriteLine("");
+            
+            Console.WriteLine("Nome do usuário:");
+                 string nome = Console.ReadLine();
+         
+            Console.WriteLine("Senha:");
+                string Senha = Console.ReadLine();
+               
+            while(nome == Senha){   //Constato erro e volto a pedir a senha 
+                Console.WriteLine ("A senha não pode ser igual o Nome de Usuário");
+                Console.WriteLine ("Por favor digite novamente"); 
+
+                Console.WriteLine("Senha:");
+                Senha = Console.ReadLine();
+            }
+
+             Console.WriteLine("Cadastro finalizado");
+
+            
+
+        }
+    }
+}
